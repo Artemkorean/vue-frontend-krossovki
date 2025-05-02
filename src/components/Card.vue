3213bfa73e12469c817893e2cedd55d762f02700
Artem Titov <artialbk@gmail.com>
2025-05-02 06:04:07 +0300

    выровнил карточки товара

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,40 +17,42 @@ defineProps({
 </script>
 
 <template>
-  <div>
-      <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
-        <img
-        v-if="isFavorites"
-        src="/close.svg"
-        alt="Удалить"
-        class="absolute top-2 right-2 w-6 h-6 cursor-pointer opacity-50 hover:opacity-100 z-10"
-        @click.stop="onClickRemove"
-        />
+  <div class="h-full">
+    <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl h-full flex flex-col">
+      <img
+      v-if="isFavorites"
+      src="/close.svg"
+      alt="Удалить"
+      class="absolute top-2 right-2 w-6 h-6 cursor-pointer opacity-50 hover:opacity-100 z-10"
+      @click.stop="onClickRemove"
+      />
 
-        <img
-          v-if="onClickFavorite"
-          :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
-          alt="like-1"
-          @click="onClickFavorite"
-          class="absolute top-8 left-8"
-        />
-        <img :src="imageUrl" alt="sneaker-1">
-
-        <p class=" mt-2">{{ title }}</p>
-
-        <div class=" flex justify-between mt-5">
-          <div class="flex flex-col">
-            <span class="text-slate-200">Цена:</span>
-            <b>{{ price }} руб.</b>
-          </div>
-
-          <img
-            v-if="onClickAdd"
-            @click="onClickAdd"
-            :src="!isAdded ? '/plus.svg' : '/checked.svg'"
-            alt="plus"
-          />
-        </div>
+      <img
+        v-if="onClickFavorite"
+        :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+        alt="like-1"
+        @click.stop="onClickFavorite"
+        class="absolute top-8 left-8"
+      />
+      <div class="mb-4 flex justify-center items-center" style="height: 160px;">
+        <img :src="imageUrl" :alt="title" class="max-h-full max-w-full object-contain">
       </div>
+
+      <p class="mt-2 text-sm font-semibold line-clamp-2 min-h-[40px]">{{ title }}</p>
+
+      <div class=" flex justify-between mt-5">
+        <div class="flex flex-col">
+          <span class="text-slate-200">Цена:</span>
+          <b>{{ price }} руб.</b>
+        </div>
+
+        <img
+          v-if="onClickAdd"
+          @click.stop="onClickAdd"
+          :src="!isAdded ? '/plus.svg' : '/checked.svg'"
+          alt="plus"
+        />
+      </div>
+    </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -12,7 +12,7 @@ const emit = defineEmits(['addToFavorites','addToCart','removeFromFavorites'])
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+  <div class="grid grid-cols-4 gap-5 auto-rows-fr" v-auto-animate>
     <Card
       v-for="item in items"
       :key="item.id"
